ad74abd7f2d7c0a776d961c5af8ef59fef3e0947
Lee JaeHyun <myleje@gmail.com>
2022-07-19 13:27:45 +0900

    Project Init

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    // 가장 먼저 호출되는 함수(메소드), 1회 호출   
+    void Awake()
+    {
+
+    }
+
+    //
+    private void OnEnable()
+    {
+
+    }
+
+    // 변수의 초기화(게임의 초기화 로직), 1회 호출
+    void Start()
+    {
+
+    }
+
+    // 화면을 렌더링하는 주기마다 호출(랜더링 주기)
+    // 불규칙한 호출 주기
+    void Update()
+    {
+
+    }
+
+    // 물리엔진의 계산주기 0.02sec
+    // 규칙적인 호출 주기
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update 함수가 끝난 후 호출
+    void LateUpdate()
+    {
+
+    }
+}
