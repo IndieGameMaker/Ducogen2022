1b9f0f5b8ff324d0f62b3299a60db3f84af46a27
Lee JaeHyun <myleje@gmail.com>
2022-07-20 13:45:39 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -12,7 +12,22 @@ public class FireCtrl : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // 게임오브젝트 또는 프리팹을 생성하는 메소드
+            // Instantiate(생성할객체, 위치, 각도)
             Instantiate(bulletPrefab, firePos.position, firePos.rotation);
         }
     }
 }
+
+
+/* 충돌관련 Call Back Function (Event)
+    if IsTrigger = false
+    OnCollisionEnter    -> 1
+    OnCollisionStay     -> n
+    OnCollisionExit     -> 1
+
+    if IsTrigger = true
+    OnTriggerEnter
+    OnTriggerStay
+    OnTriggerExit    
+
+*/
---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    // 충돌이 발생했을 때 한번 호출되는 콜백함수
+    void OnCollisionEnter(Collision coll)
+    {
+        // 충돌한 객체가 Bullet인지 여부를 확인
+        if (coll.gameObject.tag == "BULLET")
+        {
+            // 총알 삭제
+            Destroy(coll.gameObject);
+        }
+    }
+}
