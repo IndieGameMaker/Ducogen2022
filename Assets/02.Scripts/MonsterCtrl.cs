5c0d0ec2c8196022d2ac7fbf06a55ec7a9eedacb
Lee JaeHyun <myleje@gmail.com>
2022-07-21 16:03:11 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -20,6 +20,14 @@ public class MonsterCtrl : MonoBehaviour
     // 주인공의 Transform
     private Transform playerTr;
 
+    // 추적 사정거리
+    public float traceDist = 10.0f;
+    // 공격 사정거리
+    public float attackDist = 2.0f;
+
+    // 몬스터의 사망여부
+    public bool isDie = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,9 +41,16 @@ public class MonsterCtrl : MonoBehaviour
         // }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CheckMonsterState()
     {
+        while (!isDie)
+        {
+            // 몬스터와 주인공간의 거리를 계산
+            float dist = Vector3.Distance(monsterTr.position, playerTr.position);
 
+
+            yield return new WaitForSeconds(0.3f);
+        }
     }
+
 }
