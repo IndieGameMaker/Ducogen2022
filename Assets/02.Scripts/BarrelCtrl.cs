b535471a7b3eef0e3c8556146d65cd3838f33f6b
Lee JaeHyun <myleje@gmail.com>
2022-07-21 09:39:54 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -8,10 +8,18 @@ public class BarrelCtrl : MonoBehaviour
     // 텍스처를 저장하기 위한 배열
     public Texture[] textures;
 
-    // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponentInChildren<MeshRenderer>();
+        int idx = Random.Range(0, textures.Length); // (0, 3) 0,1,2
+
+        renderer.material.mainTexture = textures[idx];
+
+        /*
+            Random.Range(0, 10) -> 0 ~ 9
+            Random.Range(0.0f, 10.0f) -> 0.0f ~ 10.0f
+        */
+
     }
 
 }
