f3e18123256daeb9fa0a39bfe4e3f490c92279d2
Lee JaeHyun <myleje@gmail.com>
2022-07-21 09:33:25 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BarrelCtrl : MonoBehaviour
 {
     private MeshRenderer renderer;
+    // 텍스처를 저장하기 위한 배열
+    public Texture[] textures;
 
     // Start is called before the first frame update
     void Start()
